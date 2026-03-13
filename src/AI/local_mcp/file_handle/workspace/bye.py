872142ba67941479bc9f1b21a0cbf75e641175a7
Harsh Zazadiya <harshzazadiya13@gmail.com>
2026-03-13 17:06:43 +0530

    removed some extra files

--- src/AI/local_mcp/file_handle/workspace/bye.py
+++
@@ -1,7 +0,0 @@
-def two_sum(nums):
-    num_dict = {}
-    for i, num in enumerate(nums):
-        if num in num_dict:
-            return [num_dict[num], i]
-        num_dict[num] = i
-    return []
