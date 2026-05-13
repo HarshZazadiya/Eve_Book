f2a1e770400be215c9adb7d61e4e80da351e2d21
Harsh Zazadiya <148613229+HarshZazadiya@users.noreply.github.com>
2026-05-13 10:54:52 +0530

    Load DATABASE_URL from environment variables

--- src/database.py
+++ src/database.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 
 load_dotenv()
-# DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine("postgresql://postgres:postgres@postgres:5432/EventBooking")
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit = False,
